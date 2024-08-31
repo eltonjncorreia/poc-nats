f5e72579397277d70320ffb9c39ea9ef2860922e
eltonjncorreia <eltonjncorreia@gmail.com>
2024-08-31 11:00:20 -0300

    feat: add consumer

---
+++ consumer/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+func main() {
+	natsConnectionString := os.Getenv("NATS_URL")
+	queue := os.Getenv("ORDER_QUEUE")
+
+	natsURL := flag.String("nats_url", natsConnectionString, "URL connection string to Nats")
+
+	fmt.Println("Nats consumer connection ", *natsURL)
+
+	nc, err := nats.Connect(*natsURL)
+	if err != nil {
+		log.Fatal("Error to connection ", err)
+	}
+	defer nc.Drain()
+
+	subscribe, err := nc.SubscribeSync(queue)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		// ignore error message.
+		msg, _ := subscribe.NextMsg(10 * time.Second)
+
+		fmt.Printf("Recebeu mensagem: %q on subject %q\n", string(msg.Data), msg.Subject)
+	}
+
+}
